0540d0e08adf5dc0c5b1f4437c6e7ba8b8a88841
robinc1997 <45595217+robinc1997@users.noreply.github.com>
2018-12-04 13:09:16 +0100

    Add files via upload

---
+++ DataBase.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Event_Connect
+{
+    public class DataBase
+    {
+        public Boolean connectionExist { get; set; }
+        public MySqlConnection conng { get; set; }
+        public String conStringg { get; set; }
+        public DataBase()
+        {
+            try
+            {
+                String encrypted = Encrypt.EncryptString("SERVER=localhost;DATABASE=db_eventconnect;UID=root;PASSWORD=;", "ipshufiefis598497qsdqsg4184fgdg");
+                String decrypted = Encrypt.DecryptString(encrypted, "ipshufiefis598497qsdqsg4184fgdg");
+                String conString = decrypted;
+                MySqlConnection conn = new MySqlConnection(conString);
+                conng = conn;
+                conStringg = conString;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void Open()
+        {
+            try
+            {
+                conng.Open();
+                connectionExist = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public void Close()
+        {
+            try
+            {
+                conng.Close();
+                connectionExist = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Event_Connect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DataBase db = new DataBase();
+        public MainWindow()
+        {
+            InitializeComponent();
+            db.Open();
+        }
+
+        private void MainForm_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Close();
+        }
+
+        private void btn_Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Create_Click(object sender, RoutedEventArgs e)
+        {
+            CreateWindow cw = new CreateWindow();
+            cw.Show();
+        }
+    }
+}
